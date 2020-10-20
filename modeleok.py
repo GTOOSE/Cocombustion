2d98ac1fb11bf73c6d54f06df72537b81816e3ad
GTOOSE <73123979+GTOOSE@users.noreply.github.com>
2020-10-20 15:32:41 +0200

    Add files via upload

---
+++ modeleok.py
@@ -0,0 +1,87 @@
+
+
+from gurobipy import *
+
+CF = 10050000        #cout fixe en euros O&M liés à la capacité de la centrale
+eff = 0.38            #efficacité énergétique
+prod = 990000/eff
+ratiocmin = 0.1      # ratio carbone dans le LFC
+durée = 20
+
+
+Couts_Var = ['CV1', 'CV2']
+Investissements = ['Stockage', 'Séchage']
+
+
+combustible, pci, achat, vente, dispo1, dispo2, ges = multidict({
+        'charbon':  [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'Caroline-du-Sud': [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'Brésil': [25/3.6,75,43.2,GRB.INFINITY,30000,11,],
+        'Québec': [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'Canada Pacifique': [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'Portugal' : [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'Russie': [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'MA' : [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'Cévennes30' : [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'Cévennes48' : [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'Cévennes07': [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'rv1' : [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'rv2' : [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'rv3' : [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'rv4' : [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'rv5' : [25/3.6,75,43.2,GRB.INFINITY,30000,1,1],
+        'br': [25/3.6,75,43.2,GRB.INFINITY,30000,1,1]
+})
+
+boisfrais = ['MA','Cévennes30','Cévennes48','Cévennes07']
+boisrecycle = ['br']
+charbon = ['charbon']
+boistorrefie = ['Caroline-du-Sud','Brésil','Québec','Canada Pacifique','Portugal','Russie']
+residuvert =['rv1','rv2','rv3','rv4','rv5']
+ 
+def profit(c):
+    return pci[c]*eff*vente[c] - achat[c]
+
+
+def dispo_bio (provenance, année):
+    return dispo [provenance] if année <= 5 else dispo2 [provenance]
+
+
+model = Model('COCOmbustion')
+model.modelSense = GRB.MAXIMIZE
+
+C = model.addVars (combustible, 20, lb =0, vtype= GRB.CONTINUOUS)
+
+        
+
+stock = model.addVar (lb = 0, vtype = GRB.BINARY)   
+investment = model.addVar (lb = 0, vtype = GRB.CONTINUOUS)
+
+for i in range(durée):
+    #Assure la production énergétique pour chaque année i
+    model.addConstr(quicksum(pci[c]*C[c,i] for c in combustible) == prod)
+    
+    #Assure les 10% de charbon pour chaque année i 
+    model.addConstr((1-ratiocmin)*(sum(C[c,i] for c in charbon))-ratiocmin*(sum(C[c,i] for c in boisfrais)+sum(C[c,i] for c in boisrecycle)+sum(C[c,i] for c in boistorrefie)+sum(C[c,i] for c in residuvert))  >= 0)
+    
+    #Assure le stockage pour chaque jour i/365
+    model.addConstr((sum(C[c,i] for c in boisrecycle))+(sum(C[c,i] for c in boistorrefie)) + (sum(C[c,i] for c in residuvert)) <= 1500*365*(1+stock))
+    
+
+model.setObjective(quicksum(profit(c)*C[c,i] for c in combustible for i in range(20))- CF,GRB.MAXIMIZE)
+
+model.optimize()
+
+print('------OPTI ECONOMIQUE ---------')
+print()
+Obj=model.getObjective()
+Opti=Obj.getValue()
+print(f"Sur 20 ans le bénéfice est de : {Opti:.0f} euros")
+
+for c in combustible:
+    print(c, int((sum(C[c,i].x for i in range(20)))/1000),"Kt")
+
+
+
+
+
